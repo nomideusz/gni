05d765c5de469438668f0ee06b9b2727e36066a0
nomideusz <b.dymet@gmail.com>
2025-05-15 03:49:53 +0200

    Fix: Move data files to static directory for Vercel deployment

--- src/lib/data/testData.ts
+++ src/lib/data/testData.ts
@@ -9,36 +9,36 @@ export const dataFiles: DataFile[] = [
   {
     id: 'test1',
     name: 'Test 1',
-    path: '/src/lib/data/test1_12-05-2025-07-57-07_C_Analytics.dat'
+    path: '/data/test1_12-05-2025-07-57-07_C_Analytics.dat'
   },
   {
     id: 'test2',
     name: 'Test 2',
-    path: '/src/lib/data/test2_12-05-2025-07-58-56_C_Analytics.dat'
+    path: '/data/test2_12-05-2025-07-58-56_C_Analytics.dat'
   },
   {
     id: 'test3',
     name: 'Test 3',
-    path: '/src/lib/data/test3_12-05-2025-08-15-24_C_Analytics.dat'
+    path: '/data/test3_12-05-2025-08-15-24_C_Analytics.dat'
   },
   {
     id: 'test4',
     name: 'Test 4',
-    path: '/src/lib/data/test4_12-05-2025-08-19-56_C_Analytics.dat'
+    path: '/data/test4_12-05-2025-08-19-56_C_Analytics.dat'
   },
   {
     id: 'test5',
     name: 'Test 5',
-    path: '/src/lib/data/test5_12-05-2025-09-03-00_C_Analytics.dat'
+    path: '/data/test5_12-05-2025-09-03-00_C_Analytics.dat'
   },
   {
     id: 'test6',
     name: 'Test 6',
-    path: '/src/lib/data/test6_12-05-2025-09-17-45_C_Analytics.dat'
+    path: '/data/test6_12-05-2025-09-17-45_C_Analytics.dat'
   },
   {
     id: 'test7',
     name: 'Test 7',
-    path: '/src/lib/data/test7_12-05-2025-10-14-11_C_Analytics.dat'
+    path: '/data/test7_12-05-2025-10-14-11_C_Analytics.dat'
   }
 ]; 
